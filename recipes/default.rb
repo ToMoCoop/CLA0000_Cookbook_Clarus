bdcf6d0f770ef31ebb6ffc58a3b967d8a6a08cdd
Fred Thompson <fred.thompson@buildempire.co.uk>
2016-08-02 07:13:28 +0100

    CLA0003-10 - Remove the locality settings
    They seem to cause an issue on AWS.
    
    #CLA0003-10 Fixed

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,7 +3,7 @@
 # Recipe:: default
 #
 
-include_recipe 'cookbook_clarus::locale'
+#include_recipe 'cookbook_clarus::locale'
 include_recipe 'apt'
 include_recipe 'imagemagick'
 include_recipe 'sqlite'
