26e035d7f7d2edd998b84fa5676e6d896b4bfb3d
Fred Thompson <fred.thompson@buildempire.co.uk>
2016-11-07 22:48:56 +0000

    Removed the specification of the Postgres version as Ubuntu 16.04 doesnt have version 9. Set the java version to Oracles to match TeamCity's build agent requirement, but also as JDK 6 isn't on ubuntu 16.06.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,6 @@
-default['postgresql']['version'] = '9.3'
+default['java']['install_flavor'] = 'oracle'
+default['java']['jdk_version'] = '8'
+default['java']['oracle']['accept_oracle_download_terms'] = true
 
 default['cookbook_clarus']['appname'] = 'clarus'
 default['cookbook_clarus']['hostname'] = 'localhost'
