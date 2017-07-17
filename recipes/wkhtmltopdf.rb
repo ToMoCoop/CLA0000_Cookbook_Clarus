5d1c6d7c75fd04fc2f7e2a376e1ac225effe3880
Fred Thompson <fred.thompson@buildempire.co.uk>
2017-07-17 12:04:13 +0100

    Updated the mirror url for the cookbook, as the old url doesn't work.
    Upped the version number too.

--- recipes/wkhtmltopdf.rb
+++ recipes/wkhtmltopdf.rb
@@ -4,6 +4,7 @@
 #
 
 # Replace the dependency packages for Ubuntu 16.06 by replacing libssl0.9.8 with libssl1.0.0
+node.set['wkhtmltopdf-update']['mirror_url'] = "https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/#{node['wkhtmltopdf-update']['version']}/#{node['wkhtmltopdf-update']['package']}"
 node.set['wkhtmltopdf-update']['dependency_packages'] = %w(libfontconfig1 libssl1.0.0 libxext6 libxrender1 fontconfig libjpeg8 xfonts-base xfonts-75dpi)
 
 # Install wkhtmltopdf itself
