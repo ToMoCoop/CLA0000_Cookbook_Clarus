dab81c51465dfcbd0dd6a8927c552207e5dfaa2e
Fred Thompson <fred.thompson@buildempire.co.uk>
2018-01-19 12:57:01 +0000

    Should have been 0.5.4 apparently.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'fred.thompson@buildempire.co.uk'
 license          'Apache 2.0'
 description      'The Clarus server cookbook, ready for the Clarus application deployment.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.5.3'
+version          '0.5.4'
 
 recipe 'cookbook_clarus', 'The Clarus server cookbook, ready for the Clarus application deployment.'
 
