e6c543773ee6aaf3259ad5e8105915f2108ef9d2
Fred Thompson <fred.thompson@buildempire.co.uk>
2018-04-27 23:12:40 +0100

    CLA0003-970 - Data Import On Demand
    Well, functionally got it right first time, but didnt account for the files that are imported are within an app named api and then they're processed by an app named worker, so the fil path doesn't match.
    
    Solved this by moving the EFS storage out to app level and named it storage, on both api and worker.

--- recipes/directories.rb
+++ recipes/directories.rb
@@ -4,10 +4,20 @@
 #
 
 app_root = node['cookbook_clarus']['app_root']
+ftp_root = node['cookbook_clarus']['ftp_root']
 deploy_user = node['appbox']['deploy_user']
 
 # Create the base location for the shared files
 directory app_root do
+  owner deploy_user
+  group deploy_user
+  mode '0755'
+  action :create
+  recursive true
+end
+
+# Create the base location for the storage ftp files
+directory ftp_root do
   owner deploy_user
   group deploy_user
   mode '0755'
