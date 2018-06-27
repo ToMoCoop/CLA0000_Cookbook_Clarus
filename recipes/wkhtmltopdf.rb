7b75ab518039164e8d2c9504fde96ad8d2b1bb29
Fred Thompson <fred.thompson@buildempire.co.uk>
2018-06-27 11:47:16 +0100

    CLA0003-1047 - Wkhtmltopdf Repository Cookbook Switch
    CLA0003-1047 As a developer I would like the version of wkhtmltopdf to be upgraded to 0.12.4 so that we can ensure we're on the latest version of the software
    
    #CLA0003-1047 In Progress

--- recipes/wkhtmltopdf.rb
+++ recipes/wkhtmltopdf.rb
@@ -6,5 +6,11 @@
 # Replace the dependency packages for Ubuntu 16.06 by replacing libssl0.9.8 with libssl1.0.0
 node.set['wkhtmltopdf-update']['dependency_packages'] = %w(libfontconfig1 libssl1.0.0 libxext6 libxrender1 fontconfig libjpeg8 xfonts-base xfonts-75dpi)
 
+# Switched to using the out of the box wkhtmltopdf-update cookbook from https://github.com/BallisticPain/chef-wkhtmltopdf/
+# At this point the binary install script which we use tries to read a default['wkhtmltopdf-update']['package'] value which
+# for some odd readon (probably a bug) is no longer set.
+# So, to ease the burden, adding this in here:
+node.set['wkhtmltopdf-update']['package'] = "wkhtmltox_#{node.get['wkhtmltopdf-update']['version']}.trusty-amd64.deb",
+
 # Install wkhtmltopdf itself
 include_recipe "wkhtmltopdf-update"
